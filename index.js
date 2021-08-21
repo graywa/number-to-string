4cb0e2a57ea664ef1289c7e396671b37845644e4
graywa <74241140+graywa@users.noreply.github.com>
2021-08-21 15:25:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+const numberField = document.querySelector('.number-field')
+const btn = document.querySelector('.btn')
+const btnModal = document.querySelector('.btn__modal')
+const modal = document.querySelector('.modal')
+const modalContent = document.querySelector('.modal__content')
+
+
+//вешаем на кнопку обработчик преобразования числа в слова
+btn.addEventListener('click', () => {
+  let string = numberField.value
+  const result = convertFullNumToWords(string)
+  modal.classList.add('active')
+  modalContent.innerHTML = `<p>${result}</p>`
+})
+
+//скрываем модальное окно
+btnModal.addEventListener('click', () => {
+  modal.classList.remove('active')
+  numberField.value = ''
+})
+
+ //преобразуем числа в слова
+function convertFullNumToWords(strNum) {
+  const suffixes = ['', 'тысяч', 'миллионов', 'миллиардов', 'триллионов', 'квадрильонов', 'квинтильонов', 'секстильнов']
+  const strNumArr = []
+  const result = []
+  let minus = ''
+  strNum = strNum.trim()
+
+  //обработка ошибок
+  if (!Number.isInteger(+strNum) || strNum === '') return 'Пожалуйста, введите целое число'
+  if (+strNum > 9e23 || +strNum < -9e23) return 'Это слишком большое число'
+
+  if (strNum[0] === '-') {
+    strNum = strNum.slice(1)
+    minus = 'минус '
+  }
+  strNum = parseInt(strNum).toString()
+  if (strNum === '0') return 'ноль'
+
+  //разбиваем строку на группы по три числа
+  while (strNum.length > 3) {
+    let dig1 = strNum[strNum.length - 1]
+    let dig2 = strNum[strNum.length - 2]
+    let dig3 = strNum[strNum.length - 3]
+    strNumArr.push(dig3 + dig2 + dig1)
+    strNum = strNum.slice(0, strNum.length - 3)
+  }
+  strNumArr.push(strNum)
+
+  for (let i = strNumArr.length - 1; i >= 0; i--) {
+    result.push(convertThreeNumToWords(strNumArr[i]) + ' ' + suffixes[i])
+  }
+
+  return minus + result.join(' ')
+}
+
+//преобразуем группу из трех чисел в слова
+function convertThreeNumToWords(numStr) {
+  const ones = ['', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять',
+    'десять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать',
+    'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать']
+  const tens = ['', '', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто']
+  const hundreds = ['', 'сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот']
+
+  let words = []
+
+  // если число меньше 20
+  if (numStr < 20) {
+    words.push(ones[+numStr])
+    return words.join(' ')
+  }
+
+  // если число больше 99
+  if (numStr.length === 3) {
+    words.push(hundreds[numStr[0]])
+    if (numStr[1] + numStr[2] < 20) {
+      words.push(ones[+(numStr[1] + numStr[2])])
+      return words.join(' ')
+    }
+    words.push(tens[numStr[1]])
+    words.push(ones[numStr[2]])
+    return words.join(' ')
+  }
+
+  // если число больше 19 и меньше 100
+  words.push(tens[numStr[0]])
+  words.push(ones[numStr[1]])
+  return words.join(' ')
+}
+
+
